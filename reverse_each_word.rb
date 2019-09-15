5e86afbe1d38280776fc9a6c2b4036580ff79119
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-15 01:09:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 include "pry"
 def reverse_each_word(string)
  array = Array.new 
- array = string.split(" ")
+ array = string.to_ary
  binding.pry
  array.collect { |word, index| 
     array[index.to_i] = word.reverse
