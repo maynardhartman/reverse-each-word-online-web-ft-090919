cd2db6e5d7a475fc3fa37a4d6c1eb479211d156c
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-15 01:19:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,7 @@
 
 def reverse_each_word(string)
- array = Array.new 
- array = string.to_ary
-
- array.collect { |word| 
-    array = word.reverse
- }
+  split_string = string.split("")
+  reversed = []
+  string.size.times { reversed << split_string.pop }
+  reversed.join
 end
